20535c8f6bb7df98f1391b90077fe953a8514a8f
ClaudiaCristinaTomus <98586557+ClaudiaCristinaTomus@users.noreply.github.com>
2022-09-07 13:02:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -86,12 +86,4 @@ var swiper = new Swiper(".home-slider", {
 
 
 
-  function loader(){
-    document.querySelector('.loader-container').classList.add('fade-out');
-  }
-  
-  function fadeOut(){
-    setInterval(loader, 1000);
-  }
-  
-  window.onload = fadeOut;
+ 
